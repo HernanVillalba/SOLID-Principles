6519cbab725d9e867485d037d5b7ae0009093504
Hernan Villalba <hernan.villalba@urbetrack.com>
2022-10-17 18:10:46 -0300

    add Logger class to log messages

---
+++ ArdalisRating/Utils/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ArdalisRating.Utils
+{
+    public static class Logger
+    {
+        public static void Log<ClassType>(string message, [CallerMemberName] string memberName = null) where ClassType : class
+        {
+            Console.WriteLine(value: $"{typeof(ClassType).Name}.{memberName}() - Message: {message}");
+        }
+    }
+}
